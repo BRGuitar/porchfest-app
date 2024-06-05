f72203646944f77dab4cdde5681cfbdd9166e36a
Brett Rich <richbrett12@gmail.com>
2024-06-05 11:36:44 -0600

    Adding Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Bebas_Neue, Lilita_One } from 'next/font/google';
+import { Analytics } from '@vercel/analytics/react';
 import './globals.css';
 
 const bebas = Bebas_Neue({ weight: '400', subsets: ['latin'] });
@@ -19,6 +20,7 @@ export default function RootLayout({
     <html lang='en'>
       <body className={bebas.className}>
         {children}
+        <Analytics />
       </body>
     </html>
   );
