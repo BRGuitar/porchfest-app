c0454ad4546f314b64c6bac064c87651c102adbe
Brett Rich <richbrett12@gmail.com>
2024-06-20 07:59:10 -0600

    Updated Landing Page with next year's date

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -2,13 +2,13 @@ export default function Home() {
   return (
     <main className='flex w-3/4 flex-col items-center pt-24 text-center'>
       <div className='w-fit rounded-xl bg-base-orange px-6 pb-1 pt-4 text-8xl text-white'>
-        Porchfest is Back!
+        See You Next Year!
       </div>
       <div className='w-fit rounded-b-xl bg-dark-blue px-6 pb-2 pt-3 text-5xl text-white'>
-        Saturday, June 8th
+        Saturday, May 31st
       </div>
       <div className='w-fit rounded-b-xl bg-white px-6 py-2 text-4xl text-dark-blue'>
-        1:45PM - 7:00PM
+        Porchfest 2025
       </div>
     </main>
   );
