35ff279f38a8e3bbb744367a0730b6af9440c9a8
Brett Rich <richbrett12@gmail.com>
2025-02-06 17:49:37 -0700

    Updated Home page banner message

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -2,13 +2,13 @@ export default function Home() {
   return (
     <main className='flex w-3/4 flex-col items-center pt-24 text-center'>
       <div className='w-fit rounded-xl bg-base-orange px-6 pb-1 pt-4 text-8xl text-white'>
-        See You Next Year!
+        Porchfest is Back!
       </div>
       <div className='w-fit rounded-b-xl bg-dark-blue px-6 pb-2 pt-3 text-5xl text-white'>
         Saturday, May 31st
       </div>
-      <div className='w-fit rounded-b-xl bg-white px-6 py-2 text-4xl text-dark-blue'>
-        Porchfest 2025
+      <div className='w-fit rounded-b-xl bg-white px-6 py-2 text-5xl text-dark-blue'>
+        2025
       </div>
     </main>
   );
