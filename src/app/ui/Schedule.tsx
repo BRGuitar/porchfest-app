52846e91fe2e460c6ea95a4cb0ad96cc8c1d5879
Brett Rich <richbrett12@gmail.com>
2025-02-11 15:19:06 -0700

    Update to admin page

--- src/app/ui/Schedule.tsx
+++ src/app/ui/Schedule.tsx
@@ -24,7 +24,7 @@ export default async function ScheduleCards({ adminMode, schedule }) {
   return schedule.map((item: any, index) => {
     return (
       <tr className={lilita.className} key={index}>
-        <td className='border-b-4 border-black px-3 text-base text-dark-blue'>
+        <td className='border-b-4 border-black bg-white px-3 text-base text-dark-blue'>
           <div className='text-4xl'>
             <div className='h-7'>
               {item.starttime.toLocaleTimeString().replace(':00 PM', '')}
@@ -32,7 +32,7 @@ export default async function ScheduleCards({ adminMode, schedule }) {
             <div className='text-lg'>PM</div>
           </div>
         </td>
-        <td className=' w-3/4 px-4 py-2 text-center text-4xl text-base-orange lg:text-5xl'>
+        <td className=' w-3/4 bg-white px-4 py-2 text-center text-4xl text-base-orange lg:text-5xl'>
           <div className='flex flex-col text-dark-blue'>
             {item.pagelink !== null ? (
               <Link
@@ -47,7 +47,7 @@ export default async function ScheduleCards({ adminMode, schedule }) {
             <div className='text-lg text-black'>{item.location}</div>
           </div>
         </td>
-        <td className='border-b-4 border-black px-3 text-right text-base text-dark-blue'>
+        <td className='border-b-4 border-black bg-white px-3 text-right text-base text-dark-blue'>
           <div className='text-4xl'>
             <div className='h-7'>
               {item.endtime.toLocaleTimeString().replace(':00 PM', '')}
