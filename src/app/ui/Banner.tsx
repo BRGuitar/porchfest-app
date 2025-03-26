0f1e407c4464faca6f69e316f59d1da6b0aa2e20
Brett Rich <richbrett12@gmail.com>
2025-03-26 15:56:56 -0600

    Adding 2025 Poster

--- src/app/ui/Banner.tsx
+++ src/app/ui/Banner.tsx
@@ -2,6 +2,7 @@
 import clsx from 'clsx';
 import Image from 'next/image';
 import Link from 'next/link';
+import posterImg from '../../../public/2025_Poster.png';
 
 export default function Banner() {
   const linkStyle = () =>
@@ -16,33 +17,44 @@ export default function Banner() {
             className='rounded-sm border-2 border-white'
             alt='Freight Street Band'
             src='/freightstreet.jpeg'
-            height={250}
-            width={250}
+            height={200}
+            width={200}
           />
           <div className='h-5 p-1 text-xl'>2025</div>
-          <div className='p-1 text-5xl'>Band List</div>
+          <div className='p-1 text-4xl lg:text-5xl'>Band List</div>
         </Link>
-        <Link href='/home/map' className={'mx-12 ' + linkStyle()}>
+        <Link href='/home/map' className={'ml-6 ' + linkStyle()}>
           <Image
             className='rounded-sm border-2 border-white'
             alt='2025 Porchfest Map Thumbnail'
             src='/PorchfestMap_Small.png'
-            height={250}
-            width={250}
+            height={200}
+            width={200}
           />
           <div className='h-5 p-1 text-xl'>2025</div>
-          <div className='p-1 text-5xl'>Porch Map</div>
+          <div className='p-1 text-4xl lg:text-5xl'>Porch Map</div>
         </Link>
-        <Link href='/home/schedule' className={linkStyle()}>
+        <Link href='/home/schedule' className={'mx-6 ' + linkStyle()}>
           <Image
             className='rounded-sm border-2 border-white'
             alt='2024 Porchfest Crowd'
             src='/lineup.png'
-            height={250}
-            width={250}
+            height={200}
+            width={200}
           />
           <div className='h-5 p-1 text-xl'>2025</div>
-          <div className='p-1 text-5xl'>Schedule</div>
+          <div className='p-1 text-4xl lg:text-5xl'>Schedule</div>
+        </Link>
+        <Link href={posterImg.src} target='_blank' className={linkStyle()}>
+          <Image
+            className='rounded-sm border-2 border-white'
+            alt='2025 Porchfest Poster'
+            src='/2025_Poster_Preview.png'
+            height={200}
+            width={200}
+          />
+          <div className='h-5 p-1 text-xl'>2025</div>
+          <div className='p-1 text-4xl lg:text-5xl'>Poster</div>
         </Link>
       </div>
     </div>
